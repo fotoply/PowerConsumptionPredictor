3320a92c8271169a1b07734a3a84d1538202b0e2
fotoply <fotoply@gmail.com>
2018-03-10 00:35:00 +0100

    First commit

---
+++ src/main/linearTempVsPower.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+
+def loadDataFromCSV(filePath):
+    return np.loadtxt(fname=filePath, delimiter=",", skiprows=1, usecols=(1, 2))
+
+def run():
+    data = loadDataFromCSV("../../data/building1retail.csv")
+
+    # Split the data into training/testing sets
+    temperature = data[:, np.newaxis, 0]
+    testSize = 200
+    x_train = temperature[:-testSize]
+    x_test = temperature[-testSize:]
+
+    power = data[:, np.newaxis, 1]
+    y_train = power[:-testSize]
+    y_test = power[-testSize:]
+
+    regressor = linear_model.LinearRegression()
+    regressor.fit(x_train, y_train)
+
+    y_prediction = regressor.predict(x_test);
+
+    # The coefficients
+    print('Coefficients: \n', regressor.coef_)
+    # The mean squared error
+    print("Mean squared error: %.2f"
+          % mean_squared_error(y_test, y_prediction))
+    # Explained variance score: 1 is perfect prediction
+    print('Variance score: %.2f' % r2_score(y_test, y_prediction))
+
+    plt.scatter(x_test, y_test, color='black')
+    plt.plot(x_test, y_prediction, color='blue', linewidth=3)
+
+    plt.xticks(())
+    plt.yticks(())
+
+    plt.show()
+
+run()
+
